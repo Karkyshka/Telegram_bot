adc2f7a2845621b4cc0030a59c8362d29e9468ca
Екатерина Хорльская <karkyshakarka@yandex.ru>
2023-04-11 20:06:47 +0300

    Черновик. сообщения уодят в чат

--- homework.py
+++ homework.py
@@ -1,11 +1,17 @@
-...
+import time
+import requests
+import telegram
+from telegram import Bot
+from telegram.ext import Updater, Filters, MessageHandler
+import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('MY_PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('MY_TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('MY_TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,46 +26,52 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    """Проверяем доступность переменных окружения."""
+    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+        return True
+    return False
 
 
 def send_message(bot, message):
-    ...
+    """Отправляем сообщение в Telegram чат."""
+    bot = Bot(token=TELEGRAM_TOKEN)
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(timestamp):
-    ...
+    """Делаем запрос к эндпоинту API-сервиса."""
+    timestamp = {'from_date': 0}
+    homework_statuses = requests.get(ENDPOINT,
+                                     headers=HEADERS,
+                                     params=timestamp)
+    return homework_statuses.json()
 
 
 def check_response(response):
+    """Проверяем ответ API на соответствие документации."""
+    response = requests.get(ENDPOINT).json()
     ...
 
 
 def parse_status(homework):
-    ...
-
+    """Извлекаем статус домашней работы."""
+    homework = requests.get(ENDPOINT).json()
+    homework_name = homework[0].get('homework_name')
+    verdict = homework[0].get('status')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-
-    ...
-
     while True:
         try:
-
-            ...
-
+            check_response(get_api_answer(timestamp))
+            send_message(bot, message)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+        
 
 
 if __name__ == '__main__':
---
+++ send_random_image.py
@@ -0,0 +1,25 @@
+import requests
+from telegram import Bot
+
+from pprint import pprint
+
+ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+PRACTICUM_TOKEN = 'y0_AgAAAAArM5YXAAYckQAAAADgqKX-ETl3w_GOQiKAGcqGzp6iUEClRPY'
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+TELEGRAM_TOKEN = '6126889100:AAFxQZm7JDmeNVyIDYHEhNw6sSUaE9sP6QQ' 
+CHAT_ID = '889248'
+
+payload = {'from_date': 1679418196}
+bot = Bot(token=TELEGRAM_TOKEN)
+
+homework_statuses = requests.get(ENDPOINT,
+                                 headers=HEADERS,
+                                 params=payload)
+
+def send_message(bot, message):
+    bot = Bot(token=TELEGRAM_TOKEN)
+    bot.send_message(CHAT_ID, message)
+
+
+pprint(homework_statuses.json())
+send_message(bot, homework_statuses.json())
